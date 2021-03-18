dc52f922a7cf6a43213687fa10f419e6b87933a1
anugrah2002 <anugrahnigam16@gmail.com>
2021-03-18 22:14:04 +0530

    tiktok

--- tiktok.py
+++ tiktok.py
@@ -1,26 +1,52 @@
 from TikTokApi import TikTokApi
+from moviepy.editor import VideoFileClip, concatenate_videoclips
 import os
-api = TikTokApi.get_instance()
-# If playwright doesn't work for you try to use selenium
-api = TikTokApi.get_instance(use_selenium=True)
+import uploadfiletoheroku
 
-results = 10
+NUMOFVIDEOS = 10
 
-# Since TikTok changed their API you need to use the custom_verifyFp option. 
-# In your web browser you will need to go to TikTok, Log in and get the s_v_web_id value.
-trending = api.trending(count=results, custom_verifyFp="verify_km82f9aj_jizxev2u_XCqM_4W3N_9hVV_NrqG98hejlXY")
+def downloadVideos():
+    api = TikTokApi.get_instance()
+    # If playwright doesn't work for you try to use selenium
+    api = TikTokApi.get_instance(use_selenium=True)
 
-for tiktok in trending:
-    # Prints the id of the tiktok
-    print(tiktok['id'])
-    r=api.get_Video_By_DownloadURL(tiktok['video']['downloadAddr'], language='en', proxy=None, custom_verifyFp="verify_km82f9aj_jizxev2u_XCqM_4W3N_9hVV_NrqG98hejlXY")
-    with open('download.mp4', 'wb') as f:
-        f.write(r)
-    os.listdir()
-    break
+    results = NUMOFVIDEOS
 
-print(len(trending))
+    # Since TikTok changed their API you need to use the custom_verifyFp option. 
+    # In your web browser you will need to go to TikTok, Log in and get the s_v_web_id value.
+    trending = api.trending(count=results, custom_verifyFp="verify_km82f9aj_jizxev2u_XCqM_4W3N_9hVV_NrqG98hejlXY")
+
+    if not os.path.exists('AllVideos'):
+        os.makedirs('AllVideos')
+
+    #Saving Videos
+    for tiktok in trending:
+
+        responseInBytes = api.get_Video_By_DownloadURL(tiktok['video']['downloadAddr'], language='en', proxy=None, custom_verifyFp="verify_km82f9aj_jizxev2u_XCqM_4W3N_9hVV_NrqG98hejlXY")
+        with open(r'AllVideos/video' + str(results) + '.mp4', 'wb') as f:
+            f.write(responseInBytes)
+        
+        results-=1
+        
+
+    print(len(trending))
+
+    concatenateVideoclips()
 
 
+
+def concatenateVideoclips():
+    
+    allClipsArray = [] #it will hold all the clips
+
+    for currentIndex in range(1,NUMOFVIDEOS+1):
+        allClipsArray.append(VideoFileClip(r"AllVideos/video" + str(currentIndex) + '.mp4'))
+    
+    final_clip = concatenate_videoclips(allClipsArray)
+    final_clip.write_videofile(r"AllVideos/finalVideo.mp4")
+
+    uploadfiletoheroku.uploadvideotoheroku(r'AllVideos/finalVideo.mp4')
+
+downloadVideos()
 
 #S_V_WEB_ID:- verify_km82f9aj_jizxev2u_XCqM_4W3N_9hVV_NrqG98hejlXY
---
+++ uploadfiletoheroku.py
@@ -0,0 +1,31 @@
+# from django.http import HttpResponse, JsonResponse
+import requests
+import cloudinary
+import cloudinary.uploader
+
+
+cloudinary.config(
+  cloud_name = 'drpb4oxcc',
+  api_key = '354327896332831',
+  api_secret = 'CvetvSAc2xW33TRe_CaitVsi9S8'
+)
+
+
+
+def uploadvideotoheroku(filepath):
+    try:
+        print(filepath)
+        filepath = str(filepath)
+        # filepath = filepath.replace(' ','\ ')
+        print(filepath)
+        myurl = 'http://ytserver.eu-gb.cf.appdomain.cloud/videouploadwithcloudinary/'
+        upload_data = cloudinary.uploader.upload_large(filepath)
+        videoPublicId=str(upload_data['public_id'])
+        videoUrl=str(upload_data['secure_url'])
+        print(videoPublicId)
+        print(videoUrl)
+        getdata = requests.post(myurl,data={'title':'Top Compilations of funny videos(TikTok)','videoPublicId':videoPublicId,'videoUrl':videoUrl})
+        print(getdata.text)  
+        # r=requests.post('http://lit-sierra-15246.herokuapp.com/videoupload/')
+    except Exception as e:
+        print(e)
