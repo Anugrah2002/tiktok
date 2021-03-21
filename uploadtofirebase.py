a410630adb391f3c7fbb6364a83d736397b9de5d
anugrah2002 <anugrahnigam16@gmail.com>
2021-03-21 12:47:18 +0530

    automatic push

--- uploadtofirebase.py
+++ uploadtofirebase.py
@@ -4,13 +4,13 @@ from firebase import Firebase
 def uploadfiletofirebase(filepath):
 
   firebaseConfig = {
-      apiKey: "AIzaSyBexTA9lK-ruTMWVWEFaAzSKuIrNBjZ7vs",
-      authDomain: "tiktokvideos-378aa.firebaseapp.com",
-      projectId: "tiktokvideos-378aa",
-      storageBucket: "tiktokvideos-378aa.appspot.com",
-      messagingSenderId: "745659917761",
-      appId: "1:745659917761:web:b55d2b3ec189fd4053fb40",
-      measurementId: "G-BHVJQBQC0M"
+      'apiKey': "AIzaSyBexTA9lK-ruTMWVWEFaAzSKuIrNBjZ7vs",
+      'authDomain': "tiktokvideos-378aa.firebaseapp.com",
+      'projectId': "tiktokvideos-378aa",
+      'storageBucket': "tiktokvideos-378aa.appspot.com",
+      'messagingSenderId': "745659917761",
+      'appId': "1:745659917761:web:b55d2b3ec189fd4053fb40",
+      'measurementId': "G-BHVJQBQC0M"
     }
 
 
