8c48285e83176704db26dc16d34f3eadf0b5bf02
anugrah2002 <anugrahnigam16@gmail.com>
2021-03-21 12:58:01 +0530

    automatic push

--- uploadtofirebase.py
+++ uploadtofirebase.py
@@ -6,12 +6,12 @@ def uploadfiletofirebase(filepath):
   firebaseConfig = {
       'apiKey': "AIzaSyBexTA9lK-ruTMWVWEFaAzSKuIrNBjZ7vs",
       'authDomain': "tiktokvideos-378aa.firebaseapp.com",
-      'projectId': "tiktokvideos-378aa",
       'storageBucket': "tiktokvideos-378aa.appspot.com",
-      'messagingSenderId': "745659917761",
-      'appId': "1:745659917761:web:b55d2b3ec189fd4053fb40",
-      'measurementId': "G-BHVJQBQC0M"
+      "databaseURL": "https://tiktokvideos-378aa-default-rtdb.firebaseio.com/",
     }
+
+
+    # https://tiktokvideos-378aa-default-rtdb.firebaseio.com/
 
 
 
