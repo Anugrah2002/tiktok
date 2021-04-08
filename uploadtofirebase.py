ab6535148559e4346c27e78587d94fa76be1af3f
anugrah2002 <anugrahnigam16@gmail.com>
2021-04-08 11:38:17 +0530

    automatic push

--- uploadtofirebase.py
+++ uploadtofirebase.py
@@ -1,6 +1,10 @@
 from firebase import Firebase
 from datetime import datetime
 import requests
+import pyrebase
+from pyrebase.pyrebase import storage  
+import firebase_admin
+from firebase_admin import storage as admin_storage, credentials, firestore
 
 
 def uploadfiletofirebase(filepath):
@@ -27,6 +31,9 @@ def uploadfiletofirebase(filepath):
 
   # as admin
   storage.child(filepath[:-5]+current_time+'.mp4').put(filepath)
+  buck=admin_storage.storageBucket()
+  da=buck.storageBucket.buck('AllVideos/finalVide2021-04-03 21:12:53.071381.mp4')
+  da.delete()
 
   url=storage.child(filepath[:-5]+current_time+'.mp4').get_url(1)
   print(url)
